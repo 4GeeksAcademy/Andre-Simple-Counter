d40a77e1b5d12d9452626f0da155be6b863b2956
ANDRE ABREO <96838895+AndreAbreo@users.noreply.github.com>
2023-11-13 22:47:36 +0000

    con botones

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,28 +1,68 @@
 import React, { useState } from "react";
 import SecondsCounter from "./counter.jsx";
 
-//create your first component
 const Home = () => {
-	const[Counter,setCounter] = useState(0)
-	useState(()=>{const interval = setInterval(()=>{setCounter(counter=> counter+1)},1000)
-	
-	
-	return ()=> clearInterval(interval)
-},[])
-	const regresiva = () => (setCounter(counter=> counter-1),1000)
+  const [counter, setCounter] = useState(0);
+  const [intervalId, setIntervalId] = useState(null);
 
-	return (
-		<div>
-			<SecondsCounter Seconds={Counter}></SecondsCounter>
-			<button
+  const startCounter = () => {
+    clearInterval(intervalId); // Limpiar el intervalo anterior, si existe
+
+    const newIntervalId = setInterval(() => {
+      setCounter((prevCounter) => prevCounter + 1);
+    }, 1000);
+
+    setIntervalId(newIntervalId);
+  };
+
+  const stopCounter = () => {
+    clearInterval(intervalId);
+  };
+
+  const regresiva = () => {
+    clearInterval(intervalId); // Limpiar el intervalo anterior, si existe
+
+    const newIntervalId = setInterval(() => {
+      setCounter((prevCounter) => prevCounter - 1);
+    }, 1000);
+
+    setIntervalId(newIntervalId);
+  };
+
+  // Iniciar el contador al cargar la página
+  React.useState(() => {
+    startCounter();
+  }, []); 
+
+  return (
+    <div>
+      <SecondsCounter Seconds={counter}></SecondsCounter>
+      <button
         type="button"
         className="btn btn-secondary m-3"
-        onClick={() => {regresiva}}
+        onClick={regresiva}
       >
-        regresiva
+        Regresiva
       </button>
-		</div>
-	)
+      <button
+        type="button"
+        className="btn btn-danger m-3"
+        onClick={stopCounter}
+      >
+        Detener
+      </button>
+      <button
+        type="button"
+        className="btn btn-primary m-3"
+        onClick={startCounter}
+      >
+        Iniciar
+      </button>
+    </div>
+  );
 };
 
 export default Home;
+
+
+
